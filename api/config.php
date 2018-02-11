040318b3fd3983eb6bd68a5fabeecff0b34f26b7
Ninareh Mehrabi <Ninarehm@usc.edu>
2018-02-10 16:30:39 -0800

    User fields added

--- api/config.php
+++ api/config.php
@@ -2,12 +2,9 @@
 /**
  * Database configuration
  */
-// define('DB_USERNAME', 'cosmicsys577t2');
-define('DB_USERNAME', 'melody');
-// define('DB_PASSWORD', 'Cosmic577#2');
-define('DB_PASSWORD', 'root');
-// define('DB_HOST', 'cosmicsys577t2.db.6739710.de6.hostedresource.net');
-define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'cosmicsys577t2');
+define('DB_PASSWORD', 'Cosmic577#2');
+define('DB_HOST', 'cosmicsys577t2.db.6739710.de6.hostedresource.net');
 define('DB_NAME', 'cosmicsys577t2');
 
 ?>
